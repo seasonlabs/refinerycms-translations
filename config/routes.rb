0930f17a2a44e8b2e42e732c42afdd59354efcd9
Vladimir Krylov <s6numid@gmail.com>
2010-12-11 16:53:28 +0200

    Russian translation was added English translation was fixed Some bugs were fixed to work with refinery 0.9.8.6 and Rails 3 Now everything works

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
-#Refinery::Application.routes.draw do
-#  resources :translations
+Refinery::Application.routes.draw do
+ # resources :translations
 
-#  scope(:path => 'refinery', :as => 'admin', :module => 'admin') do
-#    resources :translations do
-#      collection do
-#        post :update_positions
-#      end
-#    end
-#  end
-#end
-
-ActionController::Routing::Routes.draw do |map|
-  map.namespace(:admin, :path_prefix => 'refinery') do |admin|
-    admin.resources :translations
+  scope(:path => 'refinery', :as => 'admin', :module => 'admin') do
+    resources :translations do
+      collection do
+        post :update_positions
+      end
+    end
   end
 end
+
+# ActionController::Routing::Routes.draw do |map|
+  # resources :trasnslations
+  # map.namespace(:admin, :path_prefix => 'refinery') do |admin|
+    # admin.resources :translations
+  # end
+# end

c7833353471b242040662f0d147a988de1e45602
Adrien Giboire <adrien.giboire@gmail.com>
2011-01-25 19:12:42 +0100

    Add the generation of the migration file

--- lib/generators/refinerycms_translations_generator.rb
+++ lib/generators/refinerycms_translations_generator.rb
@@ -1,9 +1,6 @@
 class RefinerycmsTranslations < Refinery::Generators::EngineInstaller
   
   source_root File.expand_path('../../', __FILE__)
-
-  raise 'bouh'
-
   engine_name "translations"
 
 	translation_root = Pathname.new(File.expand_path(File.dirname(__FILE__) << "/../.."))

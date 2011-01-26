224405124ff0f337434498ff466e023afc67966b
Adrien Giboire <adrien.giboire@gmail.com>
2011-01-26 12:14:25 +0100

    Recfactoring of the generator fo copying the migration file

--- lib/generators/refinerycms_translations_generator.rb
+++ lib/generators/refinerycms_translations_generator.rb
@@ -3,39 +3,20 @@ class RefinerycmsTranslations < Refinery::Generators::EngineInstaller
   source_root File.expand_path('../../', __FILE__)
   engine_name "translations"
 
-	translation_root = Pathname.new(File.expand_path(File.dirname(__FILE__) << "/.."))
-	rails_root = if defined?(Rails.root)
-  	Rails.root
-	elsif defined?(RAILS_ROOT)
-	  Pathname.new(RAILS_ROOT)
-	else
-	  Pathname.new(ARGV.first)
-	end
+	translation_root = Pathname.new(File.expand_path(File.dirname(__FILE__) << "/../.."))
 
-	if rails_root.exist?
-  	[%w(db migrate)].each do |dir|
-	    rails_root.join(dir.join(File::SEPARATOR)).mkpath
-	  end
-
-	  copies = [
-	    {:from => %w(db migrate), :to => %w(db migrate), :filename => "create_translations.rb"}
-	  ]
-		copies.each do |copy|
-	    copy_from = translation_root.join(copy[:from].join(File::SEPARATOR), copy[:filename])
-		  copy_to = rails_root.join(copy[:to].join(File::SEPARATOR), copy[:filename])
-	    unless copy_to.exist?
-		    FileUtils::copy_file copy_from.to_s, copy_to.to_s
-	    else
-	      puts "'#{File.join copy[:to], copy[:filename]}' already existed in your application so your existing file was not overwritten."
-	    end
-	  end
-
-		puts "---------"
-		puts "Copied refinerycms-translation migration files."
-		puts "Now, run rake db:migrate" 
-		puts "Make sure 'i18n_frontend_translation_locales' and 'i18n_translation_default_frontend_locale' are defined to fit your needs in your Refinery Setting"
-	else
-	  puts "Please specify the path of the project that you want to use the translation with, i.e. refinerycms-translation-install /path/to/project"
-	end
+  next_migration_number = ActiveRecord::Generators::Base.next_migration_number(File.dirname(__FILE__))
   
+  copy_to = Rails.root.join("db/migrate/#{next_migration_number}_create_translations.rb")
+
+  unless copy_to.exist?
+    FileUtils::copy_file translation_root.join('db/migrate/create_translations.rb'), copy_to.to_s
+  else
+    puts "'#{File.join copy[:to], copy[:filename]}' already existed in your application so your existing file was not overwritten."
+  end
+
+	puts "------------------------"
+	puts "Copied refinerycms-translation migration files."
+	puts "Make sure 'i18n_frontend_translation_locales' and 'i18n_translation_default_frontend_locale' are defined to fit your needs in your Refinery Setting"
+
 end
